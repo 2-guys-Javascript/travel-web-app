9657e838dcc7c0966d04f6210e3e344d84ff8d4b
Programming-Seungwan <swsj4480@gmail.com>
2023-10-14 21:13:55 +0900

    Feat: make exchange component
    used exchange api and shared api key in notion document
    made JpNavBar component's header margin and padding
    make setupProxy.js file to solve CORS problem
    get http-proxy-middleware module to solve CORS

--- src/components/Japan/Exchange.jsx
+++ src/components/Japan/Exchange.jsx
@@ -1,5 +1,54 @@
+import { useEffect, useState } from 'react';
+import './JapanDefaultLayout.css';
+
 function Exchange() {
-  return <div>일본 환율 화면입니다</div>;
+  const apiKey = import.meta.env.VITE_EXCHANGE_APIKEY;
+  const [krwToJpy, setKrwToJpy] = useState(0);
+  const [jpyToKrw, setJpyToKrw] = useState(0);
+  const [isKrwToJpy, setIsKrwToJpy] = useState(true);
+
+  // 1엔이 한화로 몇 원인지를 구해주는 함수
+  async function getKrwToJpyExchange() {
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/${apiKey}/pair/jpy/krw`
+    );
+    const exchangeData = await response.json();
+
+    setKrwToJpy(exchangeData);
+  }
+
+  // 한화 1원이 일본화로 몇엔에 해당하는지를 구해주는 함수
+  async function getJpyToKrwExchange() {
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/${apiKey}/pair/krw/jpy`
+    );
+    const exchangeData = await response.json();
+
+    setJpyToKrw(exchangeData);
+  }
+
+  // 무엇을 보여줄지 바꿔주는 핸들링 함수 => 처음엔 1엔이 한화로 몇원인지, 클릭하면 반대로
+  function handleChangeKrwJpy() {
+    setIsKrwToJpy(!isKrwToJpy);
+  }
+
+  useEffect(() => {
+    getKrwToJpyExchange();
+    getJpyToKrwExchange();
+  }, []);
+
+  return (
+    <div className='exchange-outer-div'>
+      <div>일본 환율 화면입니다</div>
+      <div className='real-exchangeRate' onClick={handleChangeKrwJpy}>
+        {isKrwToJpy === true ? (
+          <div>{krwToJpy['conversion_rate']}</div>
+        ) : (
+          <div>{jpyToKrw['conversion_rate']}</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Exchange;
