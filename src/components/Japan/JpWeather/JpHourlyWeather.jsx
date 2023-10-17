80dcb5f85f2cde5fb5cc4fd73a5c30aa77ed0b26
이동준 <manymogo@kakao.com>
2023-10-18 04:55:45 +0900

    ✨FEAT : 날씨 3일 예보 + CSS 스타일링 수정

--- src/components/Japan/JpWeather/JpHourlyWeather.jsx
+++ src/components/Japan/JpWeather/JpHourlyWeather.jsx
@@ -7,9 +7,9 @@ function JpHourlyWeather({ days }) {
     <ul className='japan-weather-hourly'>
       {days[0].hour.map((hour) => (
         <li key={hour.time.slice(10, 13)}>
+          <p> {hour.time.slice(10, 13)}시 </p>
           <img className='japan-weather-hourly-icon' src={hour.condition.icon} />
-          <p> {hour.feelslike_c} </p>
-          <p> {hour.time.slice(10, 16)} </p>
+          <p> {hour.feelslike_c}° </p>
         </li>
       ))}
     </ul>
--- src/components/Korea/KrWeather/KrForecastWeather.jsx
+++ src/components/Korea/KrWeather/KrForecastWeather.jsx
@@ -1,5 +1,30 @@
-function KrForecastWeather() {
-  return <div>KrForecastWeather</div>;
+function KrForecastWeather({ days }) {
+  if (days === '') {
+    return null;
+  }
+
+  return (
+    <ul className='korea-forecast'>
+      <li>
+        <span>오늘</span>
+        <img src={days[0].day.condition.icon}></img>
+        <span>{days[0].day.mintemp_c}°</span>
+        <span>{days[0].day.maxtemp_c}°</span>
+      </li>
+      <li>
+        <span>내일</span>
+        <img src={days[1].day.condition.icon}></img>
+        <span>{days[1].day.mintemp_c}°</span>
+        <span>{days[1].day.maxtemp_c}°</span>
+      </li>
+      <li className='last'>
+        <span>모래</span>
+        <img src={days[2].day.condition.icon}></img>
+        <span>{days[2].day.mintemp_c}°</span>
+        <span>{days[2].day.maxtemp_c}°</span>
+      </li>
+    </ul>
+  );
 }
 
 export default KrForecastWeather;
